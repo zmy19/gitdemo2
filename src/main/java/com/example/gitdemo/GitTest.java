565eacc123f998105cf55642cb0e596deb96eb39
zengmy <zengmys@foxmail.com>
2023-04-12 02:29:19 +0800

    hotfix

--- src/main/java/com/example/gitdemo/GitTest.java
+++ src/main/java/com/example/gitdemo/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello1");
+        System.out.println("hello4");
         System.out.println("hello2");
+        System.out.println("helloHOtfix");
     }
 }
